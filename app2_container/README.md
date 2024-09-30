007cd7e3ffa7b92d04e32a59beba0cb9ea87a919
ankitapol10 <156779445+ankitapol10@users.noreply.github.com>
2024-09-30 12:29:56 +0530

    Delete app2_container directory

--- app2_container/README.md
+++
@@ -1,3 +0,0 @@
-# app2_container
-
-A new Flutter project.
--- app2_container/android/app/src/main/kotlin/com/example/app2_container/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.app2_container
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
